097003cc36c19d84737e9932ecef420b7db214bf
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-09-20 13:49:21 -0500

    add music project placeholders

--- components/Project/Project.js
+++ components/Project/Project.js
@@ -4,12 +4,14 @@ import Image from 'next/future/image'
 // Styles
 import styles from './Project.module.css'
 
-export default function Project({ name, image, description }) {
+export default function Project({ name, image, description, color }) {
   return (
     <>
       <Image
         src={image}
-        className={styles.image}
+        className={`${styles.image} ${color}`}
+        placeholder="blur"
+        loading="lazy"
         alt={`${name} - ${description}`}
       />
       <div className={styles.description}>
--- components/RecentWork/RecentWork.js
+++ components/RecentWork/RecentWork.js
@@ -11,51 +11,107 @@ import togetherForward from '../../public/together-forward.png'
 import robbyWebb from '../../public/robby-webb.png'
 
 export default function RecentWork() {
-  const data = [
-    {
-      id: 0,
-      name: 'Tom Ford',
-      image: tomFord,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
-    },
-    {
-      id: 1,
-      name: 'Supervoid',
-      image: supervoid,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
-    },
-    {
-      id: 2,
-      name: 'Together Forward',
-      image: togetherForward,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
-    },
-    {
-      id: 3,
-      name: 'Robby Webb',
-      image: robbyWebb,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
-    },
-  ]
+  const data = {
+    devData: [
+      {
+        id: 0,
+        name: 'Tom Ford',
+        image: tomFord,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.red}`,
+      },
+      {
+        id: 1,
+        name: 'Supervoid',
+        image: supervoid,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.red}`,
+      },
+      {
+        id: 2,
+        name: 'Together Forward',
+        image: togetherForward,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.red}`,
+      },
+      {
+        id: 3,
+        name: 'Robby Webb',
+        image: robbyWebb,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.red}`,
+      },
+    ],
+    musicData: [
+      {
+        id: 0,
+        name: 'Tom Ford',
+        image: tomFord,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.purple}`,
+      },
+      {
+        id: 1,
+        name: 'Supervoid',
+        image: supervoid,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.purple}`,
+      },
+      {
+        id: 2,
+        name: 'Together Forward',
+        image: togetherForward,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.purple}`,
+      },
+      {
+        id: 3,
+        name: 'Robby Webb',
+        image: robbyWebb,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eius repellat quidem quae dolorem soluta ducimus natus, laboriosam placeat eos dolores deleniti dolor? Similique quibusdam sed excepturi ab veniam ad.',
+        class: `${styles.purple}`,
+      },
+    ],
+  }
 
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
-        {/* TODO: track scroll position and update <span> text + styling */}
+      <div className={styles.devContent}>
         <h3>
           Recent <span>Dev</span> Projects
         </h3>
-        {data.map((project) => {
+        {data.devData.map((project) => {
           return (
             <Project
               key={project.id}
               name={project.name}
               image={project.image}
               description={project.description}
+              color={project.class}
+            />
+          )
+        })}
+      </div>
+      <div className={styles.musicContent}>
+        <h3>
+          Recent <span>Music</span> Projects
+        </h3>
+        {data.musicData.map((project) => {
+          return (
+            <Project
+              key={project.id}
+              name={project.name}
+              image={project.image}
+              description={project.description}
+              color={project.class}
             />
           )
         })}
