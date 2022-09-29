f7a0add1875a8f5880e0ac7a80d984a688a4c6d2
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-09-29 16:22:29 -0500

    show song title when playing

--- components/AudioPlayer/AudioPlayer.js
+++ components/AudioPlayer/AudioPlayer.js
@@ -16,59 +16,30 @@ import {
 // TODO: if user selects prev or next, it should only automatically play next song if a song is already playing
 export default function AudioPlayer({ activeIndex }) {
   const [expanded, setExpanded] = useState(false)
-  const [songNumber, setSongNumber] = useState(0)
+  const [trackNumber, setTrackNumber] = useState(0)
+  const [trackPlaying, setTrackPlaying] = useState(false)
+  const [trackChanged, setTrackChanged] = useState(false)
   const audioRef = useRef()
 
-  // Playlist
+  // Full track list
   const songs = [
-    '/patch-of-dirt.mp3',
-    '/peace-drink.mp3',
-    '/anything-youve-ever-known.mp3',
-    '/take-off-your-face.mp3',
+    {
+      title: 'Rover Rover - Patch of Dirt',
+      filePath: '/patch-of-dirt.mp3',
+    },
+    {
+      title: 'Rover Rover - Peace Drink',
+      filePath: '/peace-drink.mp3',
+    },
+    {
+      title: "Rover Rover - Anything You've Ever Known",
+      filePath: '/anything-youve-ever-known.mp3',
+    },
+    {
+      title: 'Josh Nussbaum - Take Off Your Face',
+      filePath: '/take-off-your-face.mp3',
+    },
   ]
-
-  // Play song
-  const play = () => {
-    if (audioRef.current) {
-      audioRef.current.play()
-    }
-  }
-
-  // Pause song
-  const pause = () => {
-    if (audioRef.current) {
-      audioRef.current.pause()
-    }
-  }
-
-  // Play currently active song (prev + next song updates state)
-  const playCurrentlyActiveSong = () => {
-    if (audioRef.current) {
-      audioRef.current.pause()
-      audioRef.current.load()
-      audioRef.current.play()
-    }
-  }
-
-  // Play previous song
-  const previousTrack = () => {
-    if (songNumber > 0) {
-      setSongNumber(songNumber - 1)
-    } else {
-      setSongNumber(3)
-    }
-    playCurrentlyActiveSong()
-  }
-
-  // Play next song
-  const skipTrack = () => {
-    if (songNumber < songs.length - 1) {
-      setSongNumber(songNumber + 1)
-    } else {
-      setSongNumber(0)
-    }
-    playCurrentlyActiveSong()
-  }
 
   // TODO: color logic should be connected to activeIndex in index.js
   const colors = [
@@ -84,20 +55,95 @@ export default function AudioPlayer({ activeIndex }) {
     ? `${styles.icon} ${styles.expanded}`
     : styles.icon
 
+  // Play song
+  const play = () => {
+    if (audioRef.current) {
+      if (!trackPlaying) {
+        audioRef.current.play()
+        setTrackPlaying(true)
+        setTrackChanged(true)
+        setTimeout(() => {
+          setTrackChanged(false)
+        }, 5000)
+      }
+    }
+  }
+
+  // Pause song
+  const pause = () => {
+    if (audioRef.current) {
+      if (trackPlaying) {
+        audioRef.current.pause()
+        setTrackPlaying(false)
+        setTrackChanged(false)
+      }
+    }
+  }
+
+  // Play currently active song (prev + next song updates state)
+  const playCurrentlyActiveSong = () => {
+    if (audioRef.current) {
+      audioRef.current.pause()
+      audioRef.current.load()
+      audioRef.current.play()
+      setTrackPlaying(true)
+      setTrackChanged(true)
+      setTimeout(() => {
+        setTrackChanged(false)
+      }, 5000)
+    }
+  }
+
+  // Play previous song
+  const previousTrack = () => {
+    if (trackNumber > 0) {
+      setTrackNumber(trackNumber - 1)
+    } else {
+      setTrackNumber(3)
+    }
+    playCurrentlyActiveSong()
+  }
+
+  // Play next song
+  const skipTrack = () => {
+    if (trackNumber < songs.length - 1) {
+      setTrackNumber(trackNumber + 1)
+    } else {
+      setTrackNumber(0)
+    }
+    playCurrentlyActiveSong()
+  }
+
   return (
-    <div className={`${styles.controls} ${colors[activeIndex]}`}>
-      <TiMediaPlay className={iconStylesLogic} onClick={play} />
-      <TiMediaPause className={iconStylesLogic} onClick={pause} />
-      <TiMediaRewind className={iconStylesLogic} onClick={previousTrack} />
-      <TiMediaFastForward className={iconStylesLogic} onClick={skipTrack} />
-      <TiNotesOutline
-        className={styles.expandIcon}
-        onClick={() => setExpanded(!expanded)}
-      />
-      <audio ref={audioRef}>
-        <source src={songs[songNumber]} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+    <>
+      <div className={`${styles.controls} ${colors[activeIndex]}`}>
+        <TiMediaPlay className={iconStylesLogic} onClick={play} />
+        <TiMediaPause className={iconStylesLogic} onClick={pause} />
+        <TiMediaRewind className={iconStylesLogic} onClick={previousTrack} />
+        <TiMediaFastForward className={iconStylesLogic} onClick={skipTrack} />
+        <TiNotesOutline
+          className={
+            trackPlaying
+              ? `${styles.expandIcon} ${styles.playing} ${colors[activeIndex]}`
+              : styles.expandIcon
+          }
+          onClick={() => setExpanded(!expanded)}
+        />
+        <audio ref={audioRef}>
+          <source src={songs[trackNumber].filePath} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+      <p
+        className={
+          trackPlaying && trackChanged
+            ? `${styles.trackTitle} ${styles.show}`
+            : `${styles.trackTitle}`
+        }
+        onAnimationEnd={() => setTrackPlaying(!trackPlaying)}
+      >
+        {songs[trackNumber].title}
+      </p>
+    </>
   )
 }
